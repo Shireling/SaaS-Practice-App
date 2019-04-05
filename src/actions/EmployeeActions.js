cb86f235e7a7e70ce5e448ffc73a0a7e94801f98
Shireling <ckramer787@gmail.com>
2019-04-05 12:47:59 -0500

    added employee listing logic

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,6 +1,10 @@
 import firebase from 'firebase';
 import {Actions} from 'react-native-router-flux';
-import {EMPLOYEE_UPDATE, EMPLOYEE_CREATE} from './types';
+import {
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
+} from './types';
 
 export const employeeUpdate = ({prop, value}) => {
   return {type: EMPLOYEE_UPDATE, payload: {prop, value}};
@@ -17,4 +21,13 @@ export const employeeCreate = ({name, phone, shift}) => {
         Actions.employeeList({type: 'reset'});
     });
   };
+};
+
+export const employeesFetch = () => dispatch => {
+  const {currentUser} = firebase.auth();
+
+  firebase.database().ref(`/users/${currentUser.uid}/employees`)
+    .on('value', snapshot => {
+      dispatch({type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val()})
+    });
 };
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,19 +1,52 @@
+import _ from 'lodash';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {connect} from 'react-redux';
+import {ListView} from 'react-native';
+import {employeesFetch} from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends React.Component {
+  componentWillMount = () => {
+    this.props.employeesFetch();
+
+    this.creatDataSource(this.props);
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    this.creatDataSource(nextProps);
+  }
+
+  creatDataSource = ({employees}) => {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !==r2
+    });
+
+    this.dataSource = ds.cloneWithRows(employees);
+  }
+
+  renderRow = employee => {
+    return <ListItem employee={employee} />
+  }
+
   render() {
+    console.log(this.props)
     return (
-      <View>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-        <Text>Employee List</Text>
-      </View>
+      <ListView
+        enableEmptySections
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+
+      />
     )
   }
 }
 
-export default EmployeeList;
+mapStateToProps = state => {
+  const employees = _.map(state.employees, (val, uid) => {
+    return {...val, uid}
+  })
+
+  return {employees}
+}
+
+export default connect(mapStateToProps, {employeesFetch})(EmployeeList);
