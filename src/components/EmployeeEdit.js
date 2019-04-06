30420509b941db54f0ea7e0a83f94be700312d3f
Shireling <ckramer787@gmail.com>
2019-04-06 12:38:18 -0500

    added employee edit functionality

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,37 +1,44 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {employeeUpdate, employeeCreate} from '../actions';
 import {Card, CardSection, Button} from './common';
 import EmployeeForm from './EmployeeForm';
+import {employeeUpdate, employeeSave} from '../actions'
 
-class EmployeeCreate extends Component {
+class EmployeeEdit extends Component {
+  componentWillMount = () => {
+    _.each(this.props.employee, (value, prop) => {
+      this.props.employeeUpdate({prop, value})
+    })
+  }
+
   onButtonPress = () => {
     const {name, phone, shift} = this.props;
 
-    this.props.employeeCreate({name, phone, shift: shift || 'Monday'});
+    this.props.employeeSave({name, phone, shift, uid: this.props.employee.uid})
   }
 
   render() {
     return (
       <Card>
-        <EmployeeForm {...this.props} />
+        <EmployeeForm />
         <CardSection>
           <Button onPress={this.onButtonPress}>
-            Create
+            Save Changes
           </Button>
         </CardSection>
       </Card>
-    );
+    )
   }
 }
 
-const mapStateToProps = (state) => {
+mapStateToProps = state => {
   const {name, phone, shift} = state.employeeForm;
 
-  return {name, phone, shift};
-};
+  return {name, phone, shift}
+}
 
 export default connect(mapStateToProps, {
   employeeUpdate,
-  employeeCreate
-})(EmployeeCreate);
+  employeeSave
+})(EmployeeEdit);
